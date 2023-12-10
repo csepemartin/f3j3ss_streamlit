e24ea900d8414047e0de49cca73f3038cd32d42e
csepemartin <91947483+csepemartin@users.noreply.github.com>
2023-12-10 19:56:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import streamlit as st
 
 goals = pd.read_csv('https://github.com/csepemartin/f3j3ss_streamlit/blob/main/Goals.csv?raw=true')
-attempts = goals = pd.read_csv('https://github.com/csepemartin/f3j3ss_streamlit/blob/main/Attempts.csv?raw=true')
+attempts = pd.read_csv('https://github.com/csepemartin/f3j3ss_streamlit/blob/main/Attempts.csv?raw=true')
 
 goals['id'] = goals['player_name'] + goals['club']
 attempts['id'] = attempts['player_name'] + attempts['club']
