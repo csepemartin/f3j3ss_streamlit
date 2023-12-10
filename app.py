42ea582c66037f6343d199041532f29ca8d095a7
csepemartin <91947483+csepemartin@users.noreply.github.com>
2023-12-10 19:47:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import streamlit as st
 
-goals = pd.read_csv('C:\Adat\Goals.csv')
-attempts = goals = pd.read_csv('C:\Adat\Attempts.csv')
+goals = pd.read_csv('https://github.com/csepemartin/f3j3ss_streamlit/blob/main/Goals.csv?raw=true')
+attempts = goals = pd.read_csv('https://github.com/csepemartin/f3j3ss_streamlit/blob/main/Attempts.csv?raw=true')
 
 goals['id'] = goals['player_name'] + goals['club']
 attempts['id'] = attempts['player_name'] + attempts['club']
